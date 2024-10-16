73c19216573687c7c4c6036df6973b116dc70e98
OlatunjiAbiodun <olatunjia889@gmail.com>
2024-10-16 23:28:09 +0100

    feat:add comment

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import logo from "../public/Logo.jpg";
 
 export default function Header() {
   return (
-    <nav className="mx-auto w-full bg-red-500 py-2 md:py-4 sticky top-0 mb-24">
+    <nav className="mx-auto w-full bg-red-500 py-2 md:py-4 sticky top-0 mb-14 md:mb-24">
      <div className="px-6">
      <Image src={logo} alt="" width={80} height={100} className="rounded-lg" />
      </div>
