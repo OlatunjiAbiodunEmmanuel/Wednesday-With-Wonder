22199435e7e81e8c06fc97b9891ca28b18b94392
OlatunjiAbiodun <olatunjia889@gmail.com>
2024-10-22 07:08:52 +0100

    navBar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,20 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import { motion } from "framer-motion";
 import logo from "../public/Logo.jpg";
 
 export default function Header() {
   return (
-    <nav className="mx-auto w-full bg-red-500 py-2 md:py-4 sticky top-0 mb-14 md:mb-24">
-     <div className="px-6">
-     <Image src={logo} alt="" width={80} height={100} className="rounded-lg" />
-     </div>
+    <nav className="flex items-center justify-center mx-auto w-full inset-0 bg-gradient-to-r from-transparent via-transparent to-[#676394] py-2 md:py-4 sticky top-0 mb-14 md:mb-24">
+      <div className="px-6">
+        <Image
+          src={logo}
+          alt="Logo"
+          width={60}
+          height={60}
+          className="rounded-lg"
+        />
+      </div>
     </nav>
   );
 }
